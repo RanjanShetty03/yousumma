fd3ed69181ae8224626f3ba40188e6f0b3bf1b41
shreyashebbar02 <shreyashebbar02@gmail.com>
2024-03-12 17:57:05 +0530

    updates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS 
 from model.extract_transcript import get_transcript, get_videoid
 from model.preprocess import preprocess_transcript
+from model.ext_summary import generate_extractive_summary
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -20,9 +21,8 @@ def summarize():
     try:
         video_link = request.form.get('video_link')
         video_id = get_videoid(video_link)
-        raw_transcript = get_transcript(video_id)
-        transcript = preprocess_transcript(raw_transcript)
-        summary = "This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary This is dummy summaryThis is dummy summary "
+        transcript = get_transcript(video_id)
+        summary = generate_extractive_summary(transcript)
         response = {
             'transcript': transcript,
             'summary': summary,
@@ -30,10 +30,9 @@ def summarize():
         }
         
         return jsonify(response)
+        
     except Exception as e:
-        return jsonify({'error': str(e),
-        'error': str(e),
-        'error': str(e)})
+        return jsonify({'error': str(e)})
 
 @app.route('/download_extension')
 def download_extension():
@@ -46,10 +45,8 @@ def get_summary():
     try:
         data = request.json
         video_id = data['video_id']
-        raw_transcript = get_transcript(video_id)
-        transcript = raw_transcript
-        raw_transcript = "hello"
-        summary = "The Rocky Mountains, sprawling across the western part of the North American continental shelf, offer a haven for outdoor enthusiasts with activities like hiking, climbing, skiing, and camping. The range's geographical extent is debated, starting north of the Pecos River in New Mexico and extending to the Canadian Provinces of Alberta and British Columbia, with differing definitions between Canada and the US. Three main sections define the Rockies: the Canadian Rockies, Yellowstone National Park, and the elevated region spanning Colorado and Utah, renowned for its numerous peaks over 14,000 feet. Yellowstone, nestled in the heart of the Rockies, showcases its volcanic origin and abundant geothermal features. The Continental Divide of the Americas traverses the Rockies, splitting the continent into Atlantic and Pacific drainage basins, with exceptions like endorheic basins and unique geological features such as Isa Lake and North Two Ocean Creek."
+        transcript = get_transcript(video_id)
+        summary = generate_extractive_summary(transcript)
         return jsonify({'transcript': transcript, 'summary': summary})
     except Exception as e:
         return jsonify({'error': str(e)})

6325fcfc3cf79e6edf74dbff4a4ab2659b03c13b
shreyashebbar02 <shreyashebbar02@gmail.com>
2024-03-12 16:52:45 +0530

    updates

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let player; // Define player variable outside the event handler
+    let player; 
 
     $('#transcriptForm').submit(function(event) {
         event.preventDefault(); 
@@ -78,15 +78,29 @@ $(document).ready(function() {
 
 
 function copyText() {
+    var copyButton = document.getElementById("copyButton");
     var textToCopy = $('.tab-pane.active').text();
+
     navigator.clipboard.writeText(textToCopy)
         .then(() => {
             console.log('Text copied successfully');
+
+            copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16"> \
+                <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/> \
+                <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/> \
+            </svg>';
+
+            setTimeout(() => {
+                copyButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16"> \
+                    <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/> \
+                </svg>';
+            }, 2000); 
         })
         .catch(err => {
             console.error('Error copying text: ', err);
         });
 }
+
 
 function downloadText() {
     var textToDownload = $('.tab-pane.active').text();
